e37ba73f77550624e0bae4b8564f8cf157414bee
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2023-08-15 16:59:50 -0300

    Estrutura do projeto - Configurando ESLint

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,16 +1,20 @@
-import "dotenv/config";
-import {z } from "zod";
+import 'dotenv/config'
+import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'prod', 'test']).default('dev'),
   PORT: z.coerce.number().default(3333),
-});
+})
 
-const _env = envSchema.safeParse(process.env);
+const _env = envSchema.safeParse(process.env)
 
 if (!_env.success) {
-  console.log('❌  Environment variables validation error ❌', '\n', _env.error.format());
-  throw new Error(_env.error.message);
+  console.log(
+    '❌  Environment variables validation error ❌',
+    '\n',
+    _env.error.format(),
+  )
+  throw new Error(_env.error.message)
 }
 
-export const env = _env.data;
+export const env = _env.data
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
-  port: env.PORT,
-  host: '127.0.0.1'
-}).then(() => console.log('🚀 Server is running🚀')).catch(err => console.log(err));
+app
+  .listen({
+    port: env.PORT,
+    host: '127.0.0.1',
+  })
+  .then(() => console.log('🚀 Server is running🚀'))
+  .catch((err) => console.log(err))
