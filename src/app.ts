e3a6bf2673672347edcd3d1e5fe9d239d17a0ef0
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2023-08-15 20:51:44 -0300

    Caso de Uso e Design Patterns - Criação de um usuário

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
+
+app.post('/users', async (request, response) => {
+  const registerUserSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(8).regex(/[a-z]/).regex(/[A-Z]/).regex(/[0-9]/),
+  })
+
+  const { name, email, password } = registerUserSchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password,
+    },
+  })
+
+  return response.status(201).send()
+})
