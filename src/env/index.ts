ce62d41204fc7e225a536e0c50de800874af136d
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2023-08-15 16:54:42 -0300

    Estrutura do projeto - Carregando variáveis ambiente

---
+++ src/env/index.ts
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import {z } from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'prod', 'test']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  console.log('❌  Environment variables validation error ❌', '\n', _env.error.format());
+  throw new Error(_env.error.message);
+}
+
+export const env = _env.data;
